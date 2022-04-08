f9c3eb9f2f95afed24c78b51d51e4863b5ce6c36
Zekarias <archisofttechnolog@gmail.com>
2022-04-07 19:48:40 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#inlude <studio.h>
+
+/**
+  *main - Entry point
+  *
+  *Return:always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
