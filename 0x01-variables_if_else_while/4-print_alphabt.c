78c5b0b332578a33f58f81c31ed426bcf0bd6289
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 17:10:08 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (; a <= 'z' ; a++)
+	{
+		if (a == 'e' || a == 'q')
+		{
+			continue;
+		}
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
