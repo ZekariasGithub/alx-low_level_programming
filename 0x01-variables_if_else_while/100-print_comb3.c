cf1961d6aa8728340250dc58bdc2df26aab83746
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 18:14:33 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,22 @@
 
 int main(void)
 {
-	int digit1,digit2;
+	int digit1, digit2;
 
 	for (digit1 = 48; digit1 <= 57; digit1++)
-        {
-            for (digit2 = digit1 + 1; digit2 <= 57; digit2++)
-            {
-                putchar(digit1);
-                putchar(digit2);
-                if (digit1 == 56 && digit2 == 57)
-                {
-                    continue;
-                }
-                putchar(',');
-                putchar(' ');
-            }
-        }
+	{
+		for (digit2 = digit1 + 1; digit2 <= 57; digit2++)
+		{
+			putchar(digit1);
+			putchar(digit2);
+			if (digit1 == 56 && digit2 == 57)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
 
 	putchar('\n');
 	return (0);
