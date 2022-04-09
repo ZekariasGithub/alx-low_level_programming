f8cbd796f1f79ecfc71cd487323d824041b0c2f1
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 17:24:00 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	for (a = 48; a <= 57 ; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
