bad6639da97a550ff0509d3f07f5c24683c33fc7
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 17:05:32 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A'; a <= 'Z'; a++)
+	{
+		putchar(a);
+	}
+
+	putchar('\n');
+	return (0);
+}
