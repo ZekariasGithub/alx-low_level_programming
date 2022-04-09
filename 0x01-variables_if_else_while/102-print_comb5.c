eb3442abbe737cc168fc7a463d6a79afc563d33c
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 18:54:29 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			putchar((digit1 % 10) + '0');
 			putchar(' ');
 			putchar((digit2 / 10) + '0');
-			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
 			if (digit1 == 88 && digit2 == 89)
 			{
 				continue;
