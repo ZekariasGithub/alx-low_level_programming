c62b68e752124366c55c910dbb1bd8a5bf6f6d67
Zekarias <archisofttechnolog@gmail.com>
2022-04-08 17:32:54 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 48; a >= 57 ; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+
+	putchar('\n');
+	return (0);
+}
