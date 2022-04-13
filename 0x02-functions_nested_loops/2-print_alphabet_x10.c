888681449d6d70f130500be60cc726104177c9ff
Zekarias <archisofttechnolog@gmail.com>
2022-04-12 19:03:26 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,12 @@
 *Return: Always 0 (Success)
  */
 
-void print_alphabet_x10(void)
-	for (int i = 0; i < 10; i++)
-	{
+void print_alphabet(void)
+{
+	char letter;
 
-			char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-			for (letter = 'a'; letter <= 'z'; letter++)
-				_putchar(letter);
-
-			_putchar('\n');
-	}
+	_putchar('\n');
+}
