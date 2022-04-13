8ae2a8b3b0c89cb6f339fadcf0488e1e3aec85b7
Zekarias <archisofttechnolog@gmail.com>
2022-04-12 18:22:18 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *main-main bloc
+  *description - this program returns _putchar.h
+  *Return 0- this program returns 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
+
+
+
