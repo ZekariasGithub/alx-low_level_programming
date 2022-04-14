435b19522a3704f9f39de5ae9c72c0482cb90ee7
Zekarias <archisofttechnolog@gmail.com>
2022-04-13 21:05:34 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_abs - computes absolute value of an integer
+*@n: the integer to be checked
+*Return: the absolute value of an integer
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
