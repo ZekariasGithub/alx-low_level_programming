5679e0d6b9656be05f6d79aa4a5d894c41d74bec
Zekarias <archisofttechnolog@gmail.com>
2022-04-14 13:39:05 -0700

    task 13

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *main - prints first 50 fibonaccci nos
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum, totSum;
+
+	for (count = 0; count < 50 || sum < 4000000; count++)
+	{
+		sum = fib1 + fib2;
+		if ((sum % 2) == 0)
+			totSum += sum;
+
+		fib1 = fib2;
+		fib2 = sum;
+		sum = fib1 + fib2;
+
+		if (totSUm >= 4000000)
+			break;
+	}
+	printf("%.of\n", totSum);
+
+	return (0);
+}
