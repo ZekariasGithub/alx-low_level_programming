e18ad3f0c327d8a2a9bcf6e6cb271f579d74d55b
Zekarias <archisofttechnolog@gmail.com>
2022-04-14 11:28:08 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*main - Entry point
+*add - computes the sum of two integers
+*Return: sum
+ */
+
+int add(int n , int m)
+{
+	int sum;
+
+	sum = n + m;
+
+	return (sum);
+}
