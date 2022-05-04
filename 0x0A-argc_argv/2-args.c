e5627f2ae0ba09292152085191af1908a5829ed0
Zekarias <archisofttechnolog@gmail.com>
2022-05-03 19:24:13 -0700

    all done

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,17 @@
-#include "holberton.h"
 #include <stdio.h>
+
 /**
- * main - prints the number of arguments.
- * @argc: argument count
- * @argv: Argument vector for values
- *
- *
- * Return: Always to 0
+ * main - Entry point
+ *@argc: number of arguments
+ *@argv: string with arguments
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0 ; i < argc ; i++)
-	{
+	(void)argv;
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	}
 	return (0);
 }
