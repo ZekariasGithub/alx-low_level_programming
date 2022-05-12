d6aeab9c4be54e324bcf8142d29e7baa538dda1a
Zekarias <archisofttechnolog@gmail.com>
2022-05-11 20:17:24 -0700

    all tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,25 @@
 #include "function_pointers.h"
-#include <stdlib.h>
+
 /**
-  * int_index - searches for an integer
-  * @array: pointer to the array
-  * @size: size of the array
-  * @cmp: pointer to the function
-  * Return: index of the first element found
-  */
+ * int_index - Function that searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: function pointer
+ * Return: index of first element that matches with `cmp`, or -1 if none found
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	i = -1;
-	if (size <= 0 || !(array) || !(cmp))
+	if (size < 1 || array == NULL || cmp == NULL)
 		return (-1);
-	while (++i < size)
+
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
+
 	return (-1);
 }
